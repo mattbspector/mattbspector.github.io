cfa820f868bc43d599852d7f6996e23cb5587b05
mattbspector <mattbspector@yahoo.com>
2015-10-01 11:56:15 -0400

    adding logo to nav

--- LineHop/script.js
+++ LineHop/script.js
@@ -4,21 +4,25 @@ $(document).ready(function(){
 		$('html,body').animate({
 			scrollTop: $("#LogoMain").offset().top - 58
 		}, 600);
+		return false;
 	});
 	$(".WhereWeAre").click(function(){
 		$('html,body').animate({
 			scrollTop: $("#WhereWeAreMain").offset().top - 58
-		}, 600);		
+		}, 600);	
+		return false;	
 
 	});
 	$(".Press").click(function(){
 		$('html,body').animate({
 			scrollTop: $("#PressMain").offset().top - 58
 		}, 600);
+		return false;
 	});
 	$(".Contact").click(function(){
 		$('html,body').animate({
 			scrollTop: $("#ContactMain").offset().top - 58
 		}, 600);
+		return false;
 	});
 });
